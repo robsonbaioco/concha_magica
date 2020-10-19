706a03cc511af4d9df06b494268561405e395746
Cassio <cassiohenrique_cahe@hotmail.com>
2020-10-19 19:38:23 -0300

    Fix screen and center form

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ $(function() {
         offset: 300
     }).init();
 });
+
+var isDragging = false;
+var top        = 54;
+var right      = 60;
+$(".pino")
+.mousedown(function() {
+    isDragging = false;
+})
+.mousemove(function() {
+    isDragging = true;
+
+    top        = top - 1;
+    right      = right - 1;
+
+    $('.pino').css('top', top + 'px');
+    $('.pino').css('right', right + 'px');
+ })
+.mouseup(function() {
+    var wasDragging = isDragging;
+    isDragging      = false;
+    if (!wasDragging) {
+        $("#throbble").toggle();
+    }
+});
+
+console.log(isDragging);
